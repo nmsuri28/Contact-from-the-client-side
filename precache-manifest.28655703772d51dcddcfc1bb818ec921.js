da8fcf0b7fbdf60d779698cfc6b18a0985192858
Natosha <nmsuri28@gmail.com>
2020-02-04 09:29:41 -0600

    Updates

---
+++ precache-manifest.28655703772d51dcddcfc1bb818ec921.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13bd1ca41e5f644660aed75fd510921f",
+    "url": "/Contact-from-the-client-side/index.html"
+  },
+  {
+    "revision": "dae08eca4de56c8d98a2",
+    "url": "/Contact-from-the-client-side/static/css/main.5fb33038.chunk.css"
+  },
+  {
+    "revision": "7e28b06981db0c256e75",
+    "url": "/Contact-from-the-client-side/static/js/2.b695fa46.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Contact-from-the-client-side/static/js/2.b695fa46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dae08eca4de56c8d98a2",
+    "url": "/Contact-from-the-client-side/static/js/main.262649fd.chunk.js"
+  },
+  {
+    "revision": "2e3088cb9ccb3b5f239a",
+    "url": "/Contact-from-the-client-side/static/js/runtime-main.6a5d3c20.js"
+  },
+  {
+    "revision": "f76f0cba4f75306272baf7566b50a3b8",
+    "url": "/Contact-from-the-client-side/static/media/WallsField.com-Connection-Original.f76f0cba.jpg"
+  }
+]);
